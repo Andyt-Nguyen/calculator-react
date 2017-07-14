386ddcf5b83b1dce8cb9fd01dc5bfc29aebc1529
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2017-07-13 18:51:23 -0700

    media queries

--- calculator/src/Components/Calculator.js
+++ calculator/src/Components/Calculator.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import _ from 'lodash';
 
 
@@ -51,7 +52,7 @@ class Calculator extends Component {
 				answer = parseInt(this.state.display) * parseInt(this.state.temp)
 				break;
 			default:
-				console.log('cannot calculate');
+				answer = "Cannot Calculate"
 		}
 		this.setState({display:answer})
 	}
@@ -78,55 +79,55 @@ class Calculator extends Component {
 
 	render() {
 		return (
-			<div className="row">
-				<div className="col-6">
-					<div className="answer">
-						<h1>{this.state.display}</h1>
+				<div className="row">
+					<div className="col-md-6 col-sm-4">
+						<div className="answer">
+							<h1>{this.state.display}</h1>
+						</div>
+					</div>
+					<div className="calc-btns-container col-md-6 col-sm-8">
+						<div className="calc-nums text-center">
+
+							<div className="btn-numbers col-12">
+								<button onClick={ ()=> this.clear()} className="btn btn-primary btn-nums">AC</button>
+								<button onClick={ ()=> this.setNegative()} className="btn btn-primary btn-nums">+/-</button>
+								<button onClick={ ()=> this.setPercentage()} className="btn btn-nums btn-primary">%</button>
+								<button onClick={ ()=> this.setOperator('/')} className="btn btn-primary btn-nums">/</button>
+							</div>
+
+							<div className="col-12">
+								<Button setDisplay={this.setDisplay.bind(this)} setNum={'7'} />
+								<Button setDisplay={this.setDisplay.bind(this)} setNum={'8'} />
+								<Button setDisplay={this.setDisplay.bind(this)} setNum={'9'} />
+								<button onClick={ ()=> this.setOperator('x')} className="btn btn-primary btn-nums">x</button>
+							</div>
+
+
+							<div className="col-12">
+								<Button setDisplay={ this.setDisplay.bind(this)} setNum={'4'} />
+								<Button setDisplay={ this.setDisplay.bind(this)} setNum={'5'} />
+								<Button setDisplay={ this.setDisplay.bind(this)} setNum={'6'} />
+								<button onClick={ ()=>this.setOperator('-')} className="btn btn-primary btn-nums">-</button>
+							</div>
+
+
+							<div className="col-12">
+								<Button setDisplay={this.setDisplay.bind(this)} setNum={'1'} />
+								<Button setDisplay={this.setDisplay.bind(this)} setNum={'2'} />
+								<Button setDisplay={this.setDisplay.bind(this)} setNum={'3'} />
+								<button onClick={()=>this.setOperator('+')} className="btn btn-primary btn-nums">+</button>
+							</div>
+
+							<div className="col-12">
+								<button className="btn btn-primary zero" onClick={()=> this.setDisplay('0')}> 0 </button>
+								<Button setDisplay={this.setDisplay.bind(this)} setNum={'.'} />
+								<button onClick={ ()=> this.calculate() } className="btn btn-primary btn-nums">=</button>
+							</div>
+
+
+						</div>
 					</div>
 				</div>
-				<div className="calc-btns-container col-6">
-					<div className="calc-nums text-center">
-
-						<div className="col-12">
-							<button onClick={()=> this.clear()} className="btn btn-primary btn-nums">AC</button>
-							<button onClick={()=> this.setNegative()} className="btn btn-primary btn-nums">+/-</button>
-							<button onClick={()=> this.setPercentage()} className="btn btn-nums btn-primary">%</button>
-							<button onClick={ ()=> this.setOperator('/')} className="btn btn-primary btn-nums">/</button>
-						</div>
-
-						<div className="col-12">
-							<Button setDisplay={this.setDisplay.bind(this)} setNum={'7'} />
-							<Button setDisplay={this.setDisplay.bind(this)} setNum={'8'} />
-							<Button setDisplay={this.setDisplay.bind(this)} setNum={'9'} />
-							<button onClick={()=> this.setOperator('x')} className="btn btn-primary btn-nums">x</button>
-						</div>
-
-
-						<div className="col-12">
-							<Button setDisplay={this.setDisplay.bind(this)} setNum={'4'} />
-							<Button setDisplay={this.setDisplay.bind(this)} setNum={'5'} />
-							<Button setDisplay={this.setDisplay.bind(this)} setNum={'6'} />
-							<button onClick={()=>this.setOperator('-')} className="btn btn-primary btn-nums">-</button>
-						</div>
-
-
-						<div className="col-12">
-							<Button setDisplay={this.setDisplay.bind(this)} setNum={'1'} />
-							<Button setDisplay={this.setDisplay.bind(this)} setNum={'2'} />
-							<Button setDisplay={this.setDisplay.bind(this)} setNum={'3'} />
-							<button onClick={()=>this.setOperator('+')} className="btn btn-primary btn-nums">+</button>
-						</div>
-
-						<div className="col-12">
-							<button className="btn btn-primary zero" onClick={()=> this.setDisplay('0')}> 0 </button>
-							<Button setDisplay={this.setDisplay.bind(this)} setNum={'.'} />
-							<button onClick={ ()=> this.calculate() } className="btn btn-primary btn-nums">=</button>
-						</div>
-
-
-					</div>
-				</div>
-			</div>
 		)
 	}
 }
